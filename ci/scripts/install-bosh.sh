e1364f997fe8d753be75f8e5c9663c57f03243cc
Oleksandr Slynko <oslynko@pivotal.io>
2017-02-20 21:53:38 +0000

    Log extended BOSH CLI output
    [#139835387]

--- ci/scripts/install-bosh.sh
+++ ci/scripts/install-bosh.sh
@@ -7,6 +7,8 @@ kubo_deployment_dir="$(cd "$(dirname "$0")/../.."; pwd)"
 printenv GCP_SERVICE_ACCOUNT > key.json
 set -x
 gcloud auth activate-service-account bosh-deployer@cf-pcf-kubo.iam.gserviceaccount.com --key-file=key.json
+export BOSH_LOG_LEVEL=debug
+export BOSH_LOG_PATH="$kubo_deployment_dir/bosh.log"
 "$kubo_deployment_dir/bin/deploy_bosh" "$kubo_deployment_dir/ci/environments/gcp"
 cp "$kubo_deployment_dir/ci/environments/gcp/creds.yml" bosh-creds/
 cp "$kubo_deployment_dir/ci/environments/gcp/*.json" bosh-state/
