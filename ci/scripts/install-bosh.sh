af0d8c668c89815427e5a3d57024b1355cd8e5d3
Oleksandr Slynko <oslynko@pivotal.io>
2017-02-20 23:35:24 +0000

    Improve test scripts and config
    [#139835387]

--- ci/scripts/install-bosh.sh
+++ ci/scripts/install-bosh.sh
@@ -4,11 +4,11 @@
 
 kubo_deployment_dir="$(cd "$(dirname "$0")/../.."; pwd)"
 
-printenv GCP_SERVICE_ACCOUNT > key.json
+printenv GCP_SERVICE_ACCOUNT > "$PWD/key.json"
 set -x
-gcloud auth activate-service-account bosh-deployer@cf-pcf-kubo.iam.gserviceaccount.com --key-file=key.json
+gcloud auth activate-service-account bosh-deployer@cf-pcf-kubo.iam.gserviceaccount.com --key-file="$PWD/key.json"
 export BOSH_LOG_LEVEL=debug
 export BOSH_LOG_PATH="$kubo_deployment_dir/bosh.log"
-"$kubo_deployment_dir/bin/deploy_bosh" "$kubo_deployment_dir/ci/environments/gcp"
+"$kubo_deployment_dir/bin/deploy_bosh" "$kubo_deployment_dir/ci/environments/gcp" "$PWD/key.json"
 cp "$kubo_deployment_dir/ci/environments/gcp/creds.yml" bosh-creds/
 cp "$kubo_deployment_dir/ci/environments/gcp/*.json" bosh-state/
