b19ff086054eddf39e90e8515489006711b0d91e
Oleksandr Slynko <oslynko@pivotal.io>
2017-02-21 15:48:22 +0000

    Upload BOSH director artifacts to S3
    [#139835387]

--- ci/scripts/install-bosh.sh
+++ ci/scripts/install-bosh.sh
@@ -11,4 +11,4 @@ export BOSH_LOG_LEVEL=debug
 export BOSH_LOG_PATH="$kubo_deployment_dir/bosh.log"
 "$kubo_deployment_dir/bin/deploy_bosh" "$kubo_deployment_dir/ci/environments/gcp" "$PWD/key.json"
 cp "$kubo_deployment_dir/ci/environments/gcp/creds.yml" bosh-creds/
-cp "$kubo_deployment_dir/ci/environments/gcp/*.json" bosh-state/
+cp "$kubo_deployment_dir/ci/environments/gcp/state.json" bosh-state/
