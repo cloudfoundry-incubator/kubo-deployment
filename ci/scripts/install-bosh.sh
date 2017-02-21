4d60bc762042abc588848a407330157a1040dd2b
Jeff Johnson <jrjohnson@google.com>
2017-02-21 15:57:40 -0800

    Add destroy_bosh so CI can teardown the bosh director
    [#139835387]
    [#140249735]

--- ci/scripts/install-bosh.sh
+++ ci/scripts/install-bosh.sh
@@ -9,6 +9,13 @@ set -x
 gcloud auth activate-service-account bosh-deployer@cf-pcf-kubo.iam.gserviceaccount.com --key-file="$PWD/key.json"
 export BOSH_LOG_LEVEL=debug
 export BOSH_LOG_PATH="$kubo_deployment_dir/bosh.log"
+
+# Deploy Bosh++
 "$kubo_deployment_dir/bin/deploy_bosh" "$kubo_deployment_dir/ci/environments/gcp" "$PWD/key.json"
-cp "$kubo_deployment_dir/ci/environments/gcp/creds.yml" bosh-creds/
-cp "$kubo_deployment_dir/ci/environments/gcp/state.json" bosh-state/
+
+# TODO: Stash these creds so the destroy step can be seperate
+echo "TODO" > bosh-creds/creds.yml
+echo "TODO" > bosh-state/state.json
+
+# Destroy Bosh++
+"$kubo_deployment_dir/bin/destroy_bosh" "$kubo_deployment_dir/ci/environments/gcp" "$PWD/key.json"
