285630716f23427348b6062897580c0d7edc45e5
Meaghan Kjelland <mkjelland@google.com>
2017-02-28 23:22:15 +0000

    Quoting CA cert in destroy k8s instance ci script

--- ci/scripts/destroy-k8s-instance.sh
+++ ci/scripts/destroy-k8s-instance.sh
@@ -7,7 +7,7 @@ creds_path="${PWD}/s3-bosh-creds/creds.yml"
 export BOSH_CLIENT="bosh-admin"
 export BOSH_CLIENT_SECRET="$(bosh-cli int "$creds_path" --path /admin_password)"
 export BOSH_ENVIRONMENT=$(get_setting "${kubo_deployment_dir}/ci/environments/gcp/director.yml" /internal_ip)
-export BOSH_CA_CERT=$(bosh-cli int "${creds_path}" --path=/director_ssl/ca)
+export BOSH_CA_CERT="$(bosh-cli int "${creds_path}" --path=/director_ssl/ca)"
 
 export BOSH_LOG_LEVEL=debug
 export BOSH_LOG_PATH="${kubo_deployment_dir}/bosh.log"
