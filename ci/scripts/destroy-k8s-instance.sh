fe8bf3ba0abf36858e4f5bb54e17074ad78f3f0c
Oleksandr Slynko <oslynko@pivotal.io>
2017-02-28 15:10:54 +0000

    K8s destruction CI step
    [#139740093]
    
    Signed-off-by: Konstantin Semenov <ksemenov@pivotal.io>

--- ci/scripts/destroy-k8s-instance.sh
+++ ci/scripts/destroy-k8s-instance.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -ex
 
-kubo_deployment_dir="$(cd "$(dirname "$0")/../.."; pwd)"
-cp "$PWD/s3-bosh-creds/creds.yml" "$kubo_deployment_dir/ci/environments/gcp/"
-$kubo_deployment_dir/ci/scripts/director-environment.sh "$kubo_deployment_dir/ci/environments/gcp"
+kubo_deployment_dir="${PWD}/git-kubo-deployment"
+
+creds_path="${PWD}/s3-bosh-creds/creds.yml"
+
+export BOSH_CLIENT="bosh-admin"
+export BOSH_CLIENT_SECRET="$(bosh-cli int "$creds_path" --path /admin_password)"
+export BOSH_ENVIRONMENT=$(get_setting "${kubo_deployment_dir}/ci/environments/gcp/director.yml" /internal_ip)
+export BOSH_CA_CERT=$(bosh-cli int "${creds_path}" --path=/director_ssl/ca)
 
 export BOSH_LOG_LEVEL=debug
-export BOSH_LOG_PATH="$kubo_deployment_dir/bosh.log"
-export DEBUG=1
+export BOSH_LOG_PATH="${kubo_deployment_dir}/bosh.log"
 
-bosh-cli -e gcp -d ci-service deld
+bosh-cli -d ci-service delete-deployment
