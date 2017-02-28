d712c211d4314284b04459243d8d46f28fd92023
Meaghan Kjelland <mkjelland@google.com>
2017-02-28 21:38:54 +0000

    Adding link to GCP CF deployment docs and cleaning up CF section

---
+++ ci/scripts/deploy-k8s-workload.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -ex
+
+kubo_deployment_dir="$(cd "$(dirname "$0")/../.."; pwd)"
+
+export BOSH_LOG_LEVEL=debug
+export BOSH_LOG_PATH="$kubo_deployment_dir/bosh.log"
+export DEBUG=1
+
+$kubo_deployment_dir/bin/set_kubeconfig "$kubo_deployment_dir/ci/environments/gcp" ci-service
+kubectl create -f $kubo_deployment_dir/ci/specs/nginx.yml
+kubectl get pods --namespace=kube-system
