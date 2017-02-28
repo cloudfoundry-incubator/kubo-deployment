58046f7add48ea007f0f8a67209da488414d1f29
Meaghan Kjelland <mkjelland@google.com>
2017-02-28 00:12:16 +0000

    Uncommenting necessary line and deleting unnecessary line [#139740093]

--- ci/scripts/destroy-k8s-instance.sh
+++ ci/scripts/destroy-k8s-instance.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -ex
 
 kubo_deployment_dir="$(cd "$(dirname "$0")/../.."; pwd)"
-#cp "$PWD/s3-bosh-creds/creds.yml" "$kubo_deployment_dir/ci/environments/gcp/"
+cp "$PWD/s3-bosh-creds/creds.yml" "$kubo_deployment_dir/ci/environments/gcp/"
 $kubo_deployment_dir/ci/scripts/director-environment.sh "$kubo_deployment_dir/ci/environments/gcp"
-echo "$kubo_deployment_dir/ci/environments/gcp"
 
 export BOSH_LOG_LEVEL=debug
 export BOSH_LOG_PATH="$kubo_deployment_dir/bosh.log"
