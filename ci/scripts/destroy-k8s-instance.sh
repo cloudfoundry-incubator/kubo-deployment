e0a93d9dbd57127680604b971d21e8091f973480
Konstantin Semenov <ksemenov@pivotal.io>
2017-03-01 14:09:28 +0000

    Fix bosh_admin credentials
    [#139740093]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- ci/scripts/destroy-k8s-instance.sh
+++ ci/scripts/destroy-k8s-instance.sh
@@ -4,8 +4,8 @@ kubo_deployment_dir="${PWD}/git-kubo-deployment"
 
 creds_path="${PWD}/s3-bosh-creds/creds.yml"
 
-export BOSH_CLIENT="bosh-admin"
-export BOSH_CLIENT_SECRET="$(bosh-cli int "$creds_path" --path /admin_password)"
+export BOSH_CLIENT="bosh_admin"
+export BOSH_CLIENT_SECRET="$(bosh-cli int "$creds_path" --path /bosh_admin_client_secret)"
 export BOSH_ENVIRONMENT=$(bosh-cli int "${kubo_deployment_dir}/ci/environments/gcp/director.yml" --path /internal_ip)
 export BOSH_CA_CERT="$(bosh-cli int "${creds_path}" --path=/director_ssl/ca)"
 
