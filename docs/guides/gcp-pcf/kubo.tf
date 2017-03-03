275195b50e09394f91ca33883a2efb08b7d1f137
Meaghan Kjelland <mkjelland@google.com>
2017-03-03 20:37:49 +0000

    WIP: Terraform for setting up environment to deploy kubo using an existing PCF installation

---
+++ docs/guides/gcp-pcf/kubo.tf
@@ -0,0 +1,234 @@
+// Easier mainteance for updating GCE image string
+variable "latest_ubuntu" {
+    type = "string"
+    default = "ubuntu-1404-trusty-v20161109"
+}
+
+variable "projectid" {
+    type = "string"
+}
+
+variable "region" {
+    type = "string"
+    default = "us-east1"
+}
+
+variable "kubo-region" {
+    type = "string"
+    default = "us-west1"
+}
+
+variable "zone" {
+    type = "string"
+    default = "us-east1-d"
+}
+
+
+variable "network" {
+    type = "string"
+}
+
+variable "prefix" {
+    type = "string"
+    default = ""
+}
+
+variable "service_account_email" {
+    type = "string"
+    default = ""
+}
+
+variable "baseip" {
+    type = "string"
+    default = "10.0.0.0"
+}
+
+provider "google" {
+    project = "${var.projectid}"
+    region = "${var.region}"
+}
+
+resource "google_compute_network" "bosh" {
+  name       = "${var.prefix}bosh"
+}
+
+resource "google_compute_route" "nat-primary" {
+  name        = "${var.prefix}nat-primary"
+  dest_range  = "0.0.0.0/0"
+  network       = "${var.network}"
+  next_hop_instance = "${google_compute_instance.nat-instance-private-with-nat-primary.name}"
+  next_hop_instance_zone = "${var.zone}"
+  priority    = 800
+  tags = ["no-ip"]
+}
+
+// Subnet for the BOSH director
+resource "google_compute_subnetwork" "bosh-subnet-1" {
+  name          = "${var.prefix}bosh-${var.region}"
+  ip_cidr_range = "${var.baseip}/24"
+  network       = "${google_compute_network.bosh.self_link}"
+}
+
+// Subnet for Kubo
+resource "google_compute_subnetwork" "kubo-subnet" {
+  name          = "${var.prefix}kubo-${var.kubo_region}"
+  region        = "${var.kubo_region}"
+  ip_cidr_range = "10.0.1.0/24"
+  network       = "https://www.googleapis.com/compute/v1/projects/${var.projectid}/global/networks/${var.network}"
+}
+
+// Allow SSH to BOSH bastion
+resource "google_compute_firewall" "bosh-bastion" {
+  name    = "${var.prefix}bosh-bastion"
+  network = "${var.network}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  target_tags = ["bosh-bastion"]
+}
+
+// Allow all traffic within subnet
+resource "google_compute_firewall" "intra-subnet-open" {
+  name    = "${var.prefix}intra-subnet-open"
+  network = "${var.networks}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["1-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["1-65535"]
+  }
+
+  source_tags = ["internal"]
+}
+
+// BOSH bastion host
+resource "google_compute_instance" "bosh-bastion" {
+  name         = "${var.prefix}bosh-bastion"
+  machine_type = "n1-standard-1"
+  zone         = "${var.zone}"
+
+  tags = ["bosh-bastion", "internal"]
+
+  disk {
+    image = "${var.latest_ubuntu}"
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.bosh-subnet-1.name}"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata_startup_script = <<EOT
+#!/bin/bash
+cat > /etc/motd <<EOF
+
+
+
+
+#    #     ##     #####    #    #   #   #    #    ####
+#    #    #  #    #    #   ##   #   #   ##   #   #    #
+#    #   #    #   #    #   # #  #   #   # #  #   #
+# ## #   ######   #####    #  # #   #   #  # #   #  ###
+##  ##   #    #   #   #    #   ##   #   #   ##   #    #
+#    #   #    #   #    #   #    #   #   #    #    ####
+
+Startup scripts have not finished running, and the tools you need
+are not ready yet. Please log out and log back in again in a few moments.
+This warning will not appear when the system is ready.
+EOF
+
+apt-get update
+apt-get install -y build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt-dev libxml2-dev libssl-dev libreadline6 libreadline6-dev libyaml-dev libsqlite3-dev sqlite3 jq git unzip
+gem install bosh_cli
+curl -o /tmp/cf.tgz https://s3.amazonaws.com/go-cli/releases/v6.20.0/cf-cli_6.20.0_linux_x86-64.tgz
+tar -zxvf /tmp/cf.tgz && mv cf /usr/bin/cf && chmod +x /usr/bin/cf
+curl -o /usr/bin/bosh-init https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-0.0.96-linux-amd64
+chmod +x /usr/bin/bosh-init
+
+cat > /etc/profile.d/bosh.sh <<'EOF'
+#!/bin/bash
+# Misc vars
+export prefix=${var.prefix}
+export ssh_key_path=$HOME/.ssh/bosh
+
+# Vars from Terraform
+export subnetwork=${google_compute_subnetwork.bosh-subnet-1.name}
+export network=${var.network}
+
+
+# Vars from metadata service
+export project_id=$$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/project/project-id)
+export zone=$$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/zone)
+export zone=$${zone##*/}
+export region=$${zone%-*}
+
+# Configure gcloud
+gcloud config set compute/zone $${zone}
+gcloud config set compute/region $${region}
+EOF
+
+# Clone repo
+mkdir /share
+git clone https://github.com/cloudfoundry-incubator/bosh-google-cpi-release.git /share
+chmod -R 777 /share
+
+# Install Terraform
+wget https://releases.hashicorp.com/terraform/0.7.7/terraform_0.7.7_linux_amd64.zip
+unzip terraform*.zip -d /usr/local/bin
+rm /etc/motd
+EOT
+
+  service_account {
+    email = "${var.service_account_email}"
+    scopes = ["cloud-platform"]
+  }
+}
+
+// NAT server (primary)
+resource "google_compute_instance" "nat-instance-private-with-nat-primary" {
+  name         = "${var.prefix}nat-instance-primary"
+  machine_type = "n1-standard-1"
+  zone         = "${var.zone}"
+
+  tags = ["nat", "internal"]
+
+  disk {
+    image = "${var.latest_ubuntu}"
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.bosh-subnet-1.name}"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  can_ip_forward = true
+
+  metadata_startup_script = <<EOT
+#!/bin/bash
+sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+EOT
+}
+
+output "kubo_subnet" {
+   value = "${google_compute_subnetwork.kubo-subnet.name}"
+}
