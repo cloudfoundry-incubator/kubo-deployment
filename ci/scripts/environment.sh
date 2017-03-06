b4f994e544228a08ef6b12d46c6d32e03b3e279c
Konstantin Semenov <ksemenov@pivotal.io>
2017-03-06 14:00:42 +0000

    Fixed environment variable name
    [#140395673]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- ci/scripts/environment.sh
+++ ci/scripts/environment.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export KUBO_DEPLOYMENT_DIR="$(cd "$(dirname "$0")/../.."; pwd)"
-export ENVIRONMENT_DIR="${KUBO_DEPLOYMENT_DIR}/ci/environments/$(cat kubo-lock/name)"
+export KUBO_ENVIRONMENT_DIR="${KUBO_DEPLOYMENT_DIR}/ci/environments/$(cat kubo-lock/name)"
