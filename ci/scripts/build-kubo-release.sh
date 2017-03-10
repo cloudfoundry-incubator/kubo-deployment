2b557556bf9c050b504b452aa23cb62e8fd3740e
Konstantin Semenov <ksemenov@pivotal.io>
2017-03-10 15:58:36 +0000

    Build tarball separately from deployment + use version for it
    [#140558683]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

---
+++ ci/scripts/build-kubo-release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+[ -z "$DEBUG" ] || set -x
+
+. "$(dirname "$0")/lib/environment.sh"
+
+export BOSH_LOG_LEVEL=debug
+export BOSH_LOG_PATH="${KUBO_DEPLOYMENT_DIR}/bosh.log"
+version=$(cat kubo-version/version)
+
+cd git-kubo-release
+bosh-cli create-release --name "kubo-release" --tarball="../kubo-release/kubo-release-latest.tgz" --version=${version}
