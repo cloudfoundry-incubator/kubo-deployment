d49c686a9972b7f14c563ffa15dc797a3ee06181
Konstantin Semenov <ksemenov@pivotal.io>
2017-03-22 12:06:15 +0000

    Fixed pod-2-pod test
    [#140681955]
    
    Signed-off-by: Konstantin Semenov <ksemenov@pivotal.io>

--- ci/scripts/pod-to-pod.sh
+++ ci/scripts/pod-to-pod.sh
@@ -9,6 +9,9 @@ export DEBUG=1
 cp "$PWD/s3-service-creds/service-ci-service-creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
 cp "$PWD/s3-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
 
+bosh_ca_cert=$(bosh-cli int "${KUBO_ENVIRONMENT_DIR}/creds.yml" --path=/default_ca/ca)
+client_secret=$(bosh-cli int "${KUBO_ENVIRONMENT_DIR}/creds.yml" --path=/bosh_admin_client_secret)
+
 director_ip=$(bosh-cli int "${KUBO_ENVIRONMENT_DIR}/director.yml" --path="/internal_ip" | xargs echo -n)
 credhub login -u credhub-user -p \
   "$(bosh-cli int "${KUBO_ENVIRONMENT_DIR}/creds.yml" --path="/credhub_user_password" | xargs echo -n)" \
@@ -17,12 +20,15 @@ credhub login -u credhub-user -p \
 "${KUBO_DEPLOYMENT_DIR}/bin/set_kubeconfig" "${KUBO_ENVIRONMENT_DIR}" ci-service
 kubectl create -f "${KUBO_DEPLOYMENT_DIR}/ci/specs/guestbook.yml"
 # wait for deployment to finish
-kubectl rollout status deployment/nginx -w
+kubectl rollout status deployment/frontend -w
+kubectl rollout status deployment/redis-master -w
+kubectl rollout status deployment/redis-slave -w
 
-worker_ip=$(bosh-cli -e ${director_ip} vms | grep worker | head -n1 | awk '{print $4}' | xargs echo -n)
-curl http://${worker_ip}:30303/guestbook.php?cmd=set&key=messages&value=hellothere
-result=$(curl http://${worker_ip}:30303 | grep hellothere)
+worker_ip=$(BOSH_CLIENT=bosh_admin BOSH_CLIENT_SECRET=${client_secret} BOSH_CA_CERT="${bosh_ca_cert}" bosh-cli -e ${director_ip} vms | grep worker | head -n1 | awk '{print $4}' | xargs echo -n)
+wget -O - "http://${worker_ip}:30303/guestbook.php?cmd=set&key=messages&value=hellothere"
+result=$(wget -O - "http://${worker_ip}:30303/guestbook.php?cmd=get&key=messages" | grep hellothere)
+
 if [ -z result ] ; then
-  echo "Expected the sample guestbook to display the stored value"
+  echo "Expected the sample guest book to display the stored value"
   exit 1
 fi
