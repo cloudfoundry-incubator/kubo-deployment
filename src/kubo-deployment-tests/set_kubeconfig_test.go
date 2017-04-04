e677b037923515ab9967db75048bf084c30ac65a
Joshua Winters <jwinters@pivotal.io>
2017-04-04 16:53:58 +0100

    Add tests for wrong parameters
    [#141626951]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- src/kubo-deployment-tests/set_kubeconfig_test.go
+++ src/kubo-deployment-tests/set_kubeconfig_test.go
@@ -6,16 +6,26 @@ import (
 	"path"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 )
 
-var _ = Describe("End 2 end run", func() {
+var _ = Describe("set_kubeconfig", func() {
 
 	var kuboEnv = pathFromRoot("src/kubo-deployment-tests/resources/test_gcp")
 
-	It("should work now", func() {
+	BeforeEach(func() {
 		bash.Source(pathToScript("lib/deploy_utils"), nil)
 		bash.Source(pathToScript("set_kubeconfig"), nil)
+		bash.Source("__", func(string) ([]byte, error) {
+			return []byte(`get_setting() {
+				[ "$2" == "/external-kubo-port" ] && echo "some-port";
+				[ "$2" == "/cf-tcp-router-name" ] && echo "some-url";
+				[ "$2" == "/kubo-admin-password" ] && echo "sekret";
+				return 0;
+			}`), nil
+		})
 		bash.ExportFunc("kubectl", emptyCallback)
 		bash.ExportFunc("bosh-cli", emptyCallback)
 		bash.ExportFunc("credhub", emptyCallback)
@@ -26,10 +36,41 @@ var _ = Describe("End 2 end run", func() {
 
 		os.MkdirAll(path.Join(tmpdir, "lib"), os.FileMode(0755))
 		ioutil.WriteFile(path.Join(tmpdir, "lib/deploy_utils"), deployUtilContent, 0755)
+	})
 
-		status, err := bash.Run("main", []string{kuboEnv, "two"})
+	DescribeTable("with incorrect parameters", func(params []string) {
+		status, err := bash.Run("main", params)
 
 		Expect(err).NotTo(HaveOccurred())
-		Expect(status).To(Equal(0))
+		Expect(status).NotTo(Equal(0))
+	},
+		Entry("no params", []string{}),
+		Entry("single parameter", []string{"a"}),
+		Entry("three parameters", []string{"a", "b", "c"}),
+		Entry("with missing environemnt", []string{"/missing", "a"}),
+	)
+	Context("when correct parameters are provided", func() {
+		BeforeEach(func() {
+			status, err := bash.Run("main", []string{kuboEnv, "deployment-name"})
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(status).To(Equal(0))
+		})
+
+		It("should set cluser config on kubectl", func() {
+			Expect(stderr).To(gbytes.Say("kubectl config set-cluster deployment-name --server=https://some-url:some-port"))
+		})
+
+		It("should set credentials on kubectl", func() {
+			Expect(stderr).To(gbytes.Say("kubectl config set-credentials deployment-name-admin --token=sekret"))
+		})
+
+		It("should set context on kubectl", func() {
+			Expect(stderr).To(gbytes.Say("kubectl config set-context kubo-deployment-name --cluster=deployment-name --user=deployment-name-admin"))
+		})
+
+		It("should use context on kubectl", func() {
+			Expect(stderr).To(gbytes.Say("kubectl config use-context kubo-deployment-name"))
+		})
 	})
 })
