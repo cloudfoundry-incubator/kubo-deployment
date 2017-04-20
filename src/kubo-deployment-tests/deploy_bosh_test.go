c7932ef79c74d92ae0b343204d2f27916e9c1a0d
Konstantin Semenov <ksemenov@pivotal.io>
2017-04-20 13:54:02 +0100

    Test IaaS interaction in `bosh_deploy`
    [#141626403]
    
    Signed-off-by: Anton Korshunov <anton.korshunov@icloud.com>

--- src/kubo-deployment-tests/deploy_bosh_test.go
+++ src/kubo-deployment-tests/deploy_bosh_test.go
@@ -7,16 +7,20 @@ import (
 	"path"
 	"fmt"
 	"path/filepath"
+	"github.com/onsi/gomega/gbytes"
 )
 
 var _ = Describe("Deploy KuBOSH", func() {
-	validEnvironment := path.Join(testEnvironmentPath, "test_gcp")
+	validGcpEnvironment := path.Join(testEnvironmentPath, "test_gcp")
+	validOpenstackEnvironment := path.Join(testEnvironmentPath, "test_openstack")
 	BeforeEach(func() {
-		bash.ExportFunc("bosh-cli", emptyCallback)
 		bash.ExportFunc("credhub", emptyCallback)
 	})
 
 	Context("fails", func() {
+		BeforeEach(func() {
+			bash.ExportFunc("bosh-cli", emptyCallback)
+		})
 
 		DescribeTable("when wrong number of arguments is used", func(params []string) {
 			code, err := bash.Run(pathToScript("deploy_bosh"), params)
@@ -36,7 +40,7 @@ var _ = Describe("Deploy KuBOSH", func() {
 		})
 
 		It("is given a non-existing file", func() {
-			code, err := bash.Run(pathToScript("deploy_bosh"), []string{validEnvironment, pathFromRoot("non-existing.file")})
+			code, err := bash.Run(pathToScript("deploy_bosh"), []string{validGcpEnvironment, pathFromRoot("non-existing.file")})
 			Expect(err).NotTo(HaveOccurred())
 			Expect(code).NotTo(Equal(0))
 		})
@@ -45,6 +49,7 @@ var _ = Describe("Deploy KuBOSH", func() {
 	Context("succeeds", func() {
 		BeforeEach(func() {
 			bash.SelfPath = "invocationRecorder"
+			bash.ExportFunc("bosh-cli", emptyCallback)
 			bash.Source(pathToScript("deploy_bosh"), nil)
 			boshCliMock := filepath.Join(resourcesPath, "lib", "bosh_cli_mock.sh")
 			bash.Source(boshCliMock, nil)
@@ -58,10 +63,33 @@ var _ = Describe("Deploy KuBOSH", func() {
 		})
 
 		It("runs with a valid environment and an extra file", func() {
-			bash.Debug = true
-			code, err := bash.Run("main", []string{validEnvironment, pathFromRoot("README.md")})
+			code, err := bash.Run("main", []string{validGcpEnvironment, pathFromRoot("README.md")})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(code).To(Equal(0))
+		})
+
+		It("deploys to a GCP environment", func() {
+			code, err := bash.Run("main", []string{validGcpEnvironment, pathFromRoot("README.md")})
+			Expect(stderr).To(gbytes.Say("/bosh-deployment/gcp/cpi.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(code).To(Equal(0))
+		})
+
+		It("deploys to an Openstack environment", func() {
+			code, err := bash.Run("main", []string{validOpenstackEnvironment, pathFromRoot("README.md")})
+			Expect(stderr).To(gbytes.Say("/bosh-deployment/openstack/cpi.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(code).To(Equal(0))
+		})
+
+		It("interacts with credhub", func() {
+			code, err := bash.Run("main", []string{validGcpEnvironment, pathFromRoot("README.md")})
+			Expect(stderr).To(gbytes.Say("credhub api --skip-tls-validation -s .+:8844"))
+			Expect(stderr).To(gbytes.Say("credhub login -u credhub-user -p "))
+			Expect(stderr).To(gbytes.Say("credhub ca-get -n default"))
 			Expect(err).NotTo(HaveOccurred())
 			Expect(code).To(Equal(0))
 		})
 	})
+
 })
