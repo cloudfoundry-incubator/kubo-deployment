843d2df01f6bdc346947a097a929c79b0f516f04
Felix Reyn <freyn@pivotal.io>
2017-04-24 15:47:09 -0400

    write service account key to file in destroy-bosh
    Signed-off-by: Josh Winters <jwinters@pivotal.io>

--- ci/scripts/destroy-bosh.sh
+++ ci/scripts/destroy-bosh.sh
@@ -8,6 +8,8 @@ set -x
 export BOSH_LOG_LEVEL=debug
 export BOSH_LOG_PATH="${KUBO_DEPLOYMENT_DIR}/bosh.log"
 
+bosh-cli int kubo-lock/metadata --path=/gcp_service_account > "$PWD/key.json"
+
 cp "kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
 cp "$PWD/s3-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}"
 cp "$PWD/s3-bosh-state/state.json" "${KUBO_ENVIRONMENT_DIR}"
