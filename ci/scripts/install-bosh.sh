f41f0cd3080d3ad4ab9dbb59bd20d17f472ba088
Joshua Winters <jwinters@pivotal.io>
2017-04-24 17:34:42 +0100

    Get GCP key file from lock metadata
    [#139670355]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- ci/scripts/install-bosh.sh
+++ ci/scripts/install-bosh.sh
@@ -8,9 +8,11 @@ set -x
 export BOSH_LOG_LEVEL=debug
 export BOSH_LOG_PATH="${KUBO_DEPLOYMENT_DIR}/bosh.log"
 
+bosh-cli int kubo-lock/metadata --path=/gcp_service_account > "$PWD/key.json"
+
 cp "kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
 
-"${KUBO_DEPLOYMENT_DIR}/bin/deploy_bosh" "${KUBO_ENVIRONMENT_DIR}"
+"${KUBO_DEPLOYMENT_DIR}/bin/deploy_bosh" "${KUBO_ENVIRONMENT_DIR}" "$PWD/key.json"
 
 cp "${KUBO_ENVIRONMENT_DIR}/creds.yml" "$PWD/bosh-creds/"
 cp "${KUBO_ENVIRONMENT_DIR}/state.json" "$PWD/bosh-state/"
