851e4c09fce14dcde384f1a830a477877f6de44e
Oleksandr Slynko <oslynko@pivotal.io>
2017-04-25 11:55:42 +0100

    Removed the CI folder in favor of kubo-ci
    [#135571805]
    
    Signed-off-by: Konstantin Semenov <ksemenov@pivotal.io>

--- ci/scripts/lib/environment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export KUBO_DEPLOYMENT_DIR="$(cd "$(dirname "$0")/../.."; pwd)"
-export KUBO_ENVIRONMENT_DIR="${PWD}/environment"
-mkdir -p "${KUBO_ENVIRONMENT_DIR}"
-echo "gcp" > "${KUBO_ENVIRONMENT_DIR}"/iaas
