2a18b5e517792e26274e23d3de6dde99ddd8b330
Oleksandr Slynko <oslynko@pivotal.io>
2017-05-02 17:21:11 +0100

    Fix shellcheck issues

--- src/kubo-deployment-tests/resources/lib/invocation_recorder.sh
+++ src/kubo-deployment-tests/resources/lib/invocation_recorder.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 export callCounter=0
 invocationRecorder() {
   local in_line_count=0
   declare -a in_lines
-  while read -t0.1; do
+  while read -r -t0.1; do
     in_lines[in_line_count]="$REPLY"
-    in_line_count=$(expr ${in_line_count} + 1)
+    in_line_count=$((in_line_count + 1))
   done
-  callCounter=$(expr ${callCounter} + 1)
-  echo "[$callCounter] $@" > /dev/fd/2
+  callCounter=$((callCounter + 1))
+  echo "[$callCounter] $*" > /dev/fd/2
   if [ ${in_line_count} -gt 0 ]; then
     echo "[$callCounter received] input:" > /dev/fd/2
     printf '%s\n' "${in_lines[@]}" > /dev/fd/2
