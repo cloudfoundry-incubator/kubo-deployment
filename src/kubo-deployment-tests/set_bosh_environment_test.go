ec66a35b930ea8cf958eeb61b91135a19f70375e
Akshay Mankar <amankar@pivotal.io>
2017-06-06 16:02:08 +0100

    Add script to set BOSH environment
    Use following command to login to your BOSH director with bosh_admin
    client credentials:
    
    `BOSH_ENV=<path/to/bosh/environment> source ./bin/set_bosh_environment`
    
    [#141623859]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

---
+++ src/kubo-deployment-tests/set_bosh_environment_test.go
@@ -0,0 +1,31 @@
+package kubo_deployment_tests_test
+
+import (
+	"fmt"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Setup BOSH environment", func() {
+	var kuboEnv = filepath.Join(testEnvironmentPath, "test_gcp_with_creds")
+	It("Sets the BOSH environment", func() {
+		bash.Export("BOSH_ENV", kuboEnv)
+		Expect(bash.Source(pathToScript("set_bosh_environment"), nil)).To(Succeed())
+
+		expectedVariables := map[string]string{
+			"BOSH_ENVIRONMENT":   "internal.ip",
+			"BOSH_CLIENT":        "bosh_admin",
+			"BOSH_CLIENT_SECRET": "secret",
+			"BOSH_CA_CERT":       "I-am-a-bosh-ca-cert",
+		}
+
+		for key, value := range expectedVariables {
+			exitCode, err := bash.Run(fmt.Sprintf("echo %s=$%s", key, key), nil)
+			Expect(exitCode).To(Equal(0))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(stdout.Contents())).To(ContainSubstring(fmt.Sprintf("%s=%s", key, value)))
+		}
+	})
+})
