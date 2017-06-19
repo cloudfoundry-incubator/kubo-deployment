75c94428214580f1d95da91c08fcd3d5aaacc502
Oleksandr Slynko <oslynko@pivotal.io>
2017-06-19 16:21:10 +0100

    Download CredHub 1.0 to generate kubeconfig
    [#147441527]

--- docs/guides/gcp-cf/kubo.tf
+++ docs/guides/gcp-cf/kubo.tf
@@ -183,6 +183,9 @@ sudo curl https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.1-linux-amd64
 sudo chmod a+x /usr/bin/bosh-cli
 sudo curl -L https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl -o /usr/bin/kubectl
 sudo chmod a+x /usr/bin/kubectl
+curl -L https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/1.0.0/credhub-linux-1.0.0.tgz | tar zxv
+chmod a+x credhub
+sudo mv credhub /usr/bin
 EOT
 
   service_account {
