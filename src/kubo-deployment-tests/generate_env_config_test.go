2f00a6aecf429c085c3090e3fe3fbffc58ebf7fe
Morena De Liddo <mdeliddo@pivotal.io>
2017-11-23 17:25:28 +0000

    Temporary commit for EOD 23 Nov
    Signed-off-by: Jamie Monserrate <jmonserrate@pivotal.io>

--- src/kubo-deployment-tests/generate_env_config_test.go
+++ src/kubo-deployment-tests/generate_env_config_test.go
@@ -22,6 +22,41 @@ var _ = Describe("generate_env_config", func() {
 		os.RemoveAll("/tmp/b00t")
 	})
 
+	DescribeTable("with incorrect parameters", func(params []string) {
+		status, err := bash.Run("main", params)
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(status).To(Equal(1))
+		Expect(stdout).To(gbytes.Say("Usage:"))
+	},
+		Entry("no params", []string{}),
+		Entry("single parameter", []string{"a"}),
+		Entry("two parameters", []string{"a", "b"}),
+		Entry("invalid iaas", []string{".", "a", "invalid-iaas"}),
+	)
+
+	It("should error out if environment dir doesn't exist on disk", func(){
+		status, err := bash.Run("main", []string{"/invalid-dir", "a", "b"})
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(status).To(Equal(1))
+		Expect(stdout).To(gbytes.Say("should be an existing directory"))
+		Expect(stdout).To(gbytes.Say("Usage:"))
+	})
+
+	It("does not generate config if file already exists", func() {
+		tmpDir, err := ioutil.TempDir("", "generateEnvConfig")
+		Expect(err).ToNot(HaveOccurred())
+		_, err = ioutil.TempFile(tmpDir, "director.yml")
+		Expect(err).ToNot(HaveOccurred())
+
+		status, err := bash.Run("main", []string{tmpDir, "bosh_name", "gcp"})
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(status).To(Equal(0))
+		Expect(stdout).To(gbytes.Say("Skipping config generation"))
+	})
+
 	It("gracefully concatenates the templates", func() {
 		iaas := "aws"
 		status, _ := bash.Run("main", []string{"/tmp", "b00t", iaas})
