418f9b0932f3dfff5f157e2db6a55c0e69f24287
Jamie Monserrate <jmonserrate@pivotal.io>
2017-11-23 18:17:28 +0000

    Took test to logical conclusion

--- src/kubo-deployment-tests/generate_env_config_test.go
+++ src/kubo-deployment-tests/generate_env_config_test.go
@@ -3,11 +3,14 @@ package kubo_deployment_tests_test
 import (
 	"fmt"
 	"io/ioutil"
+	"path"
 
 	"os"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 )
 
 var _ = Describe("generate_env_config", func() {
@@ -35,7 +38,7 @@ var _ = Describe("generate_env_config", func() {
 		Entry("invalid iaas", []string{".", "a", "invalid-iaas"}),
 	)
 
-	It("should error out if environment dir doesn't exist on disk", func(){
+	It("should error out if environment dir doesn't exist on disk", func() {
 		status, err := bash.Run("main", []string{"/invalid-dir", "a", "b"})
 
 		Expect(err).NotTo(HaveOccurred())
@@ -44,17 +47,47 @@ var _ = Describe("generate_env_config", func() {
 		Expect(stdout).To(gbytes.Say("Usage:"))
 	})
 
-	It("does not generate config if file already exists", func() {
-		tmpDir, err := ioutil.TempDir("", "generateEnvConfig")
-		Expect(err).ToNot(HaveOccurred())
-		_, err = ioutil.TempFile(tmpDir, "director.yml")
-		Expect(err).ToNot(HaveOccurred())
+	Context("When target file already exists", func() {
+		var (
+			envDir string
+			err    error
+		)
 
-		status, err := bash.Run("main", []string{tmpDir, "bosh_name", "gcp"})
+		BeforeEach(func() {
+			envDir, err = ioutil.TempDir("", "generateEnvConfig")
+			Expect(err).ToNot(HaveOccurred())
 
-		Expect(err).NotTo(HaveOccurred())
-		Expect(status).To(Equal(0))
-		Expect(stdout).To(gbytes.Say("Skipping config generation"))
+			err = os.Mkdir(path.Join(envDir, "bosh_name"), 0777)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			defer os.RemoveAll(envDir)
+		})
+
+		It("does not generate config if director.yml already exists", func() {
+			pathToDirectorYml := path.Join(envDir, "bosh_name", "director.yml")
+			err = ioutil.WriteFile(pathToDirectorYml, []byte(""), 0777)
+			Expect(err).ToNot(HaveOccurred())
+
+			status, err := bash.Run("main", []string{envDir, "bosh_name", "gcp"})
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(status).To(Equal(0))
+			Expect(stdout).To(gbytes.Say("Skipping config generation because " + pathToDirectorYml + " already exists"))
+		})
+
+		It("does not generate config if director-secrets.yml already exists", func() {
+			pathToDirectorYml := path.Join(envDir, "bosh_name", "director-secrets.yml")
+			err = ioutil.WriteFile(pathToDirectorYml, []byte(""), 0777)
+			Expect(err).ToNot(HaveOccurred())
+
+			status, err := bash.Run("main", []string{envDir, "bosh_name", "gcp"})
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(status).To(Equal(0))
+			Expect(stdout).To(gbytes.Say("Skipping config generation because " + pathToDirectorYml + " already exists"))
+		})
 	})
 
 	It("gracefully concatenates the templates", func() {
