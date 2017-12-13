7c2516888a6dbe17c9fe388ffe3641abf62e8525
Iain Sproat <isproat@pivotal.io>
2017-12-13 15:08:55 +0000

    Expanding coverage of deploy_k8s_test
    [#141626585]
    
    Signed-off-by: Brenda Chan <brchan@pivotal.io>

--- src/kubo-deployment-tests/deploy_k8s_test.go
+++ src/kubo-deployment-tests/deploy_k8s_test.go
@@ -103,6 +103,132 @@ var _ = Describe("Deploy K8s", func() {
 			Expect(stderr).To(gbytes.Say("bosh-cli upload-stemcell https://s3.amazonaws.com/bosh-aws-light-stemcells/light-bosh-stemcell-3124.12-aws-xen-hvm-ubuntu-trusty-go_agent.tgz"))
 		})
 	})
+
+	Context("When release source is dev", func() {
+		It("should create and upload a release", func(){
+			createAndUploadReleaseMock := Mock("create_and_upload_release", `echo`)
+			exportBoshEnvironmentMock := Mock("export_bosh_environment", "echo")
+			cloudConfigMock := Mock("set_cloud_config", "echo")
+			deployToBoshMock := Mock("deploy_to_bosh", "echo")
+			depsMock := Mock("get_deps", "echo")
+			ApplyMocks(bash, []Gob{createAndUploadReleaseMock, exportBoshEnvironmentMock, cloudConfigMock, deployToBoshMock, depsMock})
+
+			code, err := bash.Run("main", []string{validGcpEnvironment, "deployment", "dev"})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(code).To(Equal(0))
+			Expect(stderr).To(gbytes.Say("create_and_upload_release"))
+		})
+	})
+
+	Context("When release source is public", func() {
+		It("should upload a release", func(){
+			uploadReleaseMock := Mock("upload_release", `echo -n $@`)
+			getSettingMock := Mock("get_setting", `echo url-to-public-release`)
+			exportBoshEnvironmentMock := Mock("export_bosh_environment", "echo")
+			cloudConfigMock := Mock("set_cloud_config", "echo")
+			deployToBoshMock := Mock("deploy_to_bosh", "echo")
+			depsMock := Mock("get_deps", "echo")
+			ApplyMocks(bash, []Gob{uploadReleaseMock, getSettingMock, exportBoshEnvironmentMock, cloudConfigMock, deployToBoshMock, depsMock})
+
+			code, err := bash.Run("main", []string{validGcpEnvironment, "deployment", "public"})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(code).To(Equal(0))
+			Expect(stderr).To(gbytes.Say("upload_release url-to-public-release"))
+		})
+	})
+
+	It("Should export bosh env, set cloud config and deploy", func() {
+		cloudConfigMock := Mock("set_cloud_config", "echo")
+		exportBoshEnvironmentMock := Mock("export_bosh_environment", "echo")
+		deployToBoshMock := Mock("deploy_to_bosh", "echo")
+		ApplyMocks(bash, []Gob{cloudConfigMock, exportBoshEnvironmentMock, deployToBoshMock})
+
+		depsMock := Mock("get_deps", "echo")
+		ApplyMocks(bash, []Gob{depsMock})
+
+		code, err := bash.Run("main", []string{validGcpEnvironment, "deployment", "skip"})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(code).To(Equal(0))
+		Expect(stderr).To(gbytes.Say("export_bosh_environment"))
+		Expect(stderr).To(gbytes.Say("set_cloud_config"))
+		Expect(stderr).To(gbytes.Say("deploy_to_bosh"))
+	})
+
+	Context("When apply-specs is present in the manifest", func() {
+		It("should run apply-specs errand", func() {
+			cloudConfigMock := Mock("set_cloud_config", "echo")
+			exportBoshEnvironmentMock := Mock("export_bosh_environment", "echo")
+			deployToBoshMock := Mock("deploy_to_bosh", "echo")
+			ApplyMocks(bash, []Gob{cloudConfigMock, exportBoshEnvironmentMock, deployToBoshMock})
+
+			depsMock := Mock("get_deps", "echo")
+			ApplyMocks(bash, []Gob{depsMock})
+			boshMock := MockOrCallThrough("bosh-cli", `echo -n "0"`, `! [[ "$4" =~ 'apply-specs' ]]`)
+			ApplyMocks(bash, []Gob{boshMock})
+
+			code, err := bash.Run("main", []string{validGcpEnvironment, "deployment", "skip"})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(code).To(Equal(0))
+			Expect(stderr).To(gbytes.Say("run-errand apply-specs"))
+		})
+	})
+})
+
+var _ = Describe("upload_stemcell", func() {
+	BeforeEach(func(){
+		bash.Source(pathToScript("deploy_k8s"), nil)
+		boshMock := Mock("bosh-cli", "echo")
+		ApplyMocks(bash, []Gob{boshMock})
+	})
+
+	Context("GCP", func() {
+		It("should upload a google kvm ubuntu trusty stemcell", func() {
+			code, err := bash.Run("upload_stemcell", []string{"public", "gcp"})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(code).To(Equal(0))
+			Expect(stderr).To(gbytes.Say("bosh-cli upload-stemcell"))
+			Expect(stderr).To(gbytes.Say("google-kvm-ubuntu-trusty-go_agent"))
+		})
+	})
+
+	Context("vSphere", func(){
+		It("should upload a vSphere esxi ubuntu trusty stemcell", func(){
+			code, err := bash.Run("upload_stemcell", []string{"public", "vsphere"})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(code).To(Equal(0))
+			Expect(stderr).To(gbytes.Say("bosh-cli upload-stemcell"))
+			Expect(stderr).To(gbytes.Say("vsphere-esxi-ubuntu-trusty-go_agent"))
+		})
+	})
+
+	Context("OpenStack", func(){
+		It("should upload an OpenStack kvm ubuntu trusty stemcell", func(){
+			code, err := bash.Run("upload_stemcell", []string{"public", "openstack"})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(code).To(Equal(0))
+			Expect(stderr).To(gbytes.Say("bosh-cli upload-stemcell"))
+			Expect(stderr).To(gbytes.Say("openstack-kvm-ubuntu-trusty-go_agent"))
+		})
+	})
+
+	Context("AWS", func(){
+		It("should upload an AWS xen hvm ubuntu trusty light stemcell", func(){
+			code, err := bash.Run("upload_stemcell", []string{"public", "aws"})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(code).To(Equal(0))
+			Expect(stderr).To(gbytes.Say("bosh-aws-light-stemcells"))
+			Expect(stderr).To(gbytes.Say("aws-xen-hvm-ubuntu-trusty-go_agent"))
+		})
+	})
+
+	Context("skip", func(){
+		It("should not upload a stemcell", func(){
+			code, err := bash.Run("upload_stemcell", []string{"skip", "aws"})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(code).To(Equal(0))
+			Expect(stderr).NotTo(gbytes.Say("bosh-cli upload-stemcell"))
+		})
+	})
 })
 
 var _ = Describe("get_director_uuid", func() {
