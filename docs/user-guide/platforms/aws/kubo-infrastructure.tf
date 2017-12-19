cadbf4c6cfa04857f021898d6709ba60acf8e60b
Christian Ang <cang@pivotal.io>
2017-12-19 09:56:55 +0000

    Restrict permissions for AWS master and worker users in terraform
    [#151445255]
    
    Signed-off-by: Morena De Liddo <mdeliddo@pivotal.io>

--- docs/user-guide/platforms/aws/kubo-infrastructure.tf
+++ docs/user-guide/platforms/aws/kubo-infrastructure.tf
@@ -180,18 +180,82 @@ resource "aws_iam_role_policy" "kubo-master" {
     policy = <<EOF
 {
   "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "ec2:*",
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": "elasticloadbalancing:*",
-      "Resource": "*"
-    }
-  ]
+    "Statement": [
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Action": [
+          "ec2:DescribeInstances",
+          "ec2:DescribeRouteTables",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeVolumes"
+        ],
+        "Resource": [
+          "*"
+        ]
+      },
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Action": [
+          "ec2:CreateTags",
+          "ec2:ModifyInstanceAttribute",
+          "ec2:CreateSecurityGroup",
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:RevokeSecurityGroupIngress",
+          "ec2:DeleteSecurityGroup",
+          "ec2:CreateRoute",
+          "ec2:DeleteRoute",
+          "ec2:CreateVolume",
+          "ec2:AttachVolume",
+          "ec2:DetachVolume",
+          "ec2:DeleteVolume"
+        ],
+        "Resource": [
+          "*"
+        ]
+      },
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Action": [
+          "ec2:DescribeVpcs",
+          "elasticloadbalancing:AddTags",
+          "elasticloadbalancing:AttachLoadBalancerToSubnets",
+          "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
+          "elasticloadbalancing:CreateLoadBalancer",
+          "elasticloadbalancing:CreateLoadBalancerPolicy",
+          "elasticloadbalancing:CreateLoadBalancerListeners",
+          "elasticloadbalancing:ConfigureHealthCheck",
+          "elasticloadbalancing:DeleteLoadBalancer",
+          "elasticloadbalancing:DeleteLoadBalancerListeners",
+          "elasticloadbalancing:DescribeLoadBalancers",
+          "elasticloadbalancing:DescribeLoadBalancerAttributes",
+          "elasticloadbalancing:DetachLoadBalancerFromSubnets",
+          "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+          "elasticloadbalancing:ModifyLoadBalancerAttributes",
+          "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+          "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
+          "elasticloadbalancing:AddTags",
+          "elasticloadbalancing:CreateListener",
+          "elasticloadbalancing:CreateTargetGroup",
+          "elasticloadbalancing:DeleteListener",
+          "elasticloadbalancing:DeleteTargetGroup",
+          "elasticloadbalancing:DescribeListeners",
+          "elasticloadbalancing:DescribeLoadBalancerPolicies",
+          "elasticloadbalancing:DescribeTargetGroups",
+          "elasticloadbalancing:DescribeTargetHealth",
+          "elasticloadbalancing:ModifyListener",
+          "elasticloadbalancing:ModifyTargetGroup",
+          "elasticloadbalancing:RegisterTargets",
+          "elasticloadbalancing:SetLoadBalancerPoliciesOfListener"
+        ],
+        "Resource": [
+          "*"
+        ]
+      }
+    ]
 }
 EOF
 }
@@ -229,19 +293,14 @@ resource "aws_iam_role_policy" "kubo-worker" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": "ec2:Describe*",
+      "Sid": "",
       "Effect": "Allow",
-      "Resource": "*"
-    },
-    {
-      "Action": "ec2:AttachVolume",
-      "Effect": "Allow",
-      "Resource": "*"
-    },
-    {
-      "Action": "ec2:DetachVolume",
-      "Effect": "Allow",
-      "Resource": "*"
+      "Action": [
+        "ec2:DescribeInstances"
+      ],
+      "Resource": [
+        "*"
+      ]
     }
   ]
 }
