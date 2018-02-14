34a28f8de6070352ff73106c4fbe26f96a337c17
Travis Hall <thall@vmware.com>
2018-02-13 16:09:03 -0800

    Set up Kubelet certificate bootstrapping
    * Generate a bootstrap token used during Kubelet client certificate
      bootstrapping.
    * Pass along the cluster admin credentials, used during Kubelet server
      certificate bootstrapping.
    * Remove the BOSH-generated Kubelet certificates.
    
    [#154817831]
    [#153592308]
    [#154727215]
    [#154727213]

--- src/kubo-deployment-tests/generate_manifest_test.go
+++ src/kubo-deployment-tests/generate_manifest_test.go
@@ -88,16 +88,6 @@ var _ = Describe("Generate manifest", func() {
 			Expect(pathValue).To(Equal("true"))
 		})
 
-		It("should include a variable section with tls-kubelet, tls-kubernetes", func() {
-			status, err := bash.Run("main", []string{kuboEnv, "cucumber", "director_uuid"})
-			Expect(err).NotTo(HaveOccurred())
-			Expect(status).To(Equal(0))
-
-			Expect(stdout).To(gbytes.Say("variables:"))
-			Expect(stdout).To(gbytes.Say("tls-kubelet"))
-			Expect(stdout).To(gbytes.Say("tls-kubernetes"))
-		})
-
 		It("should include an alternative name with master.cfcr.internal for the tls-kubernetes variable", func() {
 			status, err := bash.Run("main", []string{kuboEnv, "cucumber", "director_uuid"})
 			Expect(err).NotTo(HaveOccurred())
