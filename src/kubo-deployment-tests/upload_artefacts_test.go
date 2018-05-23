d17d731bd9f154207ae9531a6d111b27a27b6c91
Mark James Hender <mhender@pivotal.io>
2018-05-23 11:50:01 +0100

    Use full stemcell for AWS
    Becaue they failed to upload the light one for 3586.8
    
    [#157805711]
    
    Signed-off-by: Akshay Mankar <amankar@pivotal.io>

--- src/kubo-deployment-tests/upload_artefacts_test.go
+++ src/kubo-deployment-tests/upload_artefacts_test.go
@@ -73,7 +73,7 @@ var _ = Describe("upload_artefacts", func() {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(code).To(Equal(0))
 
-			Expect(stderr).To(gbytes.Say("bosh upload-stemcell https://s3.amazonaws.com/bosh-aws-light-stemcells/light-bosh-stemcell-3124.12-aws-xen-hvm-ubuntu-trusty-go_agent.tgz"))
+			Expect(stderr).To(gbytes.Say("bosh upload-stemcell https://s3.amazonaws.com/bosh-core-stemcells/aws/bosh-stemcell-3124.12-aws-xen-hvm-ubuntu-trusty-go_agent.tgz"))
 		})
 	})
 
@@ -149,11 +149,11 @@ var _ = Describe("upload_stemcell", func() {
 	})
 
 	Context("AWS", func() {
-		It("should upload an AWS xen hvm ubuntu trusty light stemcell", func() {
+		It("should upload an AWS xen hvm ubuntu trusty full stemcell", func() {
 			code, err := bash.Run("upload_stemcell", []string{"aws"})
 			Expect(err).NotTo(HaveOccurred())
 			Expect(code).To(Equal(0))
-			Expect(stderr).To(gbytes.Say("bosh-aws-light-stemcells"))
+			Expect(stderr).To(gbytes.Say("bosh-core-stemcells"))
 			Expect(stderr).To(gbytes.Say("aws-xen-hvm-ubuntu-trusty-go_agent"))
 		})
 	})
