7f6118dedfd83b3f5ece919176a2ed838f01132b
Vlad Stoian <vstoian@pivotal.io>
2018-08-31 14:13:50 +0100

    Allow changing vm_type of apply-addons VM
    Signed-off-by: Akshay Mankar <amankar@pivotal.io>

--- bin/test-standard-ops.sh
+++ bin/test-standard-ops.sh
@@ -19,7 +19,7 @@ test_standard_ops() {
       # BOSH
       check_interpolation "use-runtime-config-bosh-dns.yml"
       check_interpolation "rename.yml" "-v deployment_name=fubar"
-      check_interpolation "vm-types.yml" "-v master_vm_type=master" "-v worker_vm_type=worker"
+      check_interpolation "vm-types.yml" "-v master_vm_type=master" "-v worker_vm_type=worker" "-v apply_addons_vm_type=addons"
       check_interpolation "use-trusty-stemcell.yml"
       check_interpolation "add-vm-extensions-to-master.yml"
       check_interpolation "use-vm-extensions.yml" "-v deployment_name=cfcr"
