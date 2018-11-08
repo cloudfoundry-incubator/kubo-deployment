7295dec403367954f59d899ffa5da001072c9117
Oleksandr Slynko <oslynko@pivotal.io>
2018-11-08 11:21:26 +0000

    Add README & unit tests
    [#161318475]

--- bin/test-standard-ops.sh
+++ bin/test-standard-ops.sh
@@ -23,6 +23,7 @@ test_standard_ops() {
       check_interpolation "use-trusty-stemcell.yml"
       check_interpolation "add-vm-extensions-to-master.yml"
       check_interpolation "use-vm-extensions.yml" "-v deployment_name=cfcr"
+      check_interpolation "worker_count.yml" "-v worker_count=4"
 
       # Infrastructure
       check_interpolation "iaas/aws/cloud-provider.yml"
