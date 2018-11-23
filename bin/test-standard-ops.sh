17d12b3661f33d01e3b04bf57e9b71cec9ef8436
Iain Sproat <isproat@pivotal.io>
2018-11-23 13:56:26 +0000

    Remove use-trusty ops-file
    [#162168313]

--- bin/test-standard-ops.sh
+++ bin/test-standard-ops.sh
@@ -20,7 +20,6 @@ test_standard_ops() {
       check_interpolation "use-runtime-config-bosh-dns.yml"
       check_interpolation "rename.yml" "-v deployment_name=fubar"
       check_interpolation "vm-types.yml" "-v master_vm_type=master" "-v worker_vm_type=worker" "-v apply_addons_vm_type=addons"
-      check_interpolation "use-trusty-stemcell.yml"
       check_interpolation "add-vm-extensions-to-master.yml"
       check_interpolation "use-vm-extensions.yml" "-v deployment_name=cfcr"
       check_interpolation "worker_count.yml" "-v worker_count=4"
