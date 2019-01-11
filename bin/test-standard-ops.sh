1628564d690d26d59c376ad6daa509e856c397d4
Iain Sproat <isproat@pivotal.io>
2019-01-11 12:29:33 +0000

    Updates unit tests for making coredns default
    [#159739243]

--- bin/test-standard-ops.sh
+++ bin/test-standard-ops.sh
@@ -64,7 +64,7 @@ test_standard_ops() {
       check_interpolation "enable-denyescalatingexec.yml"
       check_interpolation "enable-podsecuritypolicy.yml"
       check_interpolation "enable-securitycontextdeny.yml"
-      check_interpolation "use-coredns.yml"
+      check_interpolation "use-kube-dns.yml"
       check_interpolation "enable-encryption-config.yml" "-v encryption-config=encryption-config.yml"
       check_interpolation "enable-csi-shared-mounts.yml"
 
